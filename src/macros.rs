919f4ab0a9ca98e34a41216f43b8285a5f188264
Erich Gubler <erichdongubler@gmail.com>
2018-10-09 16:56:40 -0600

    Add some spacing to macros

--- src/macros.rs
+++ src/macros.rs
@@ -1,11 +1,11 @@
 #[macro_export]
 macro_rules! ansi {
-    ($($l:expr),*) => { concat!("\x1B", $($l),*) };
+    ($($l: expr),*) => { concat!("\x1B", $($l),*) };
 }
 
 /// Shamelessly stolen from the Termion codebase. :)
 /// See here: https://en.wikipedia.org/wiki/ANSI_escape_code#CSI_sequences
 #[macro_export]
 macro_rules! csi {
-    ($($l:expr),*) => { ansi!("[", $($l),*) };
+    ($($l: expr),*) => { ansi!("[", $($l),*) };
 }
