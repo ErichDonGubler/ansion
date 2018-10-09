ed6026dc3d014e7206c4c9493751dc9fcab7fb36
Erich Gubler <erichdongubler@gmail.com>
2018-10-09 16:57:22 -0600

    Change credits for Termion codebase as comments, but not doc comments

--- src/macros.rs
+++ src/macros.rs
@@ -3,9 +3,9 @@ macro_rules! ansi {
     ($($l: expr),*) => { concat!("\x1B", $($l),*) };
 }
 
-/// Shamelessly stolen from the Termion codebase. :)
-/// See here: https://en.wikipedia.org/wiki/ANSI_escape_code#CSI_sequences
 #[macro_export]
 macro_rules! csi {
+    // Shamelessly stolen from the Termion codebase. :)
+    // See here: https://en.wikipedia.org/wiki/ANSI_escape_code#CSI_sequences
     ($($l: expr),*) => { ansi!("[", $($l),*) };
 }
